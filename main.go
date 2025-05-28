72c844f6c27dd142f1382be013685192bd78c11d
bekanay <beki.bekovich2004@gmail.com>
2025-05-28 15:12:00 +0500

    base of code and initializing go mod file

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("example.csv")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	var csvparser CSVParser
+
+	for {
+		line, err := csvparser.ReadLine(file)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading line:", err)
+			return
+		}
+		fmt.Println(line)
+	}
+}
+
+type CSVParser interface {
+	ReadLine(r io.Reader) (string, error)
+	GetField(n int) (string, error)
+	GetNumberOfFields() int
+}
+
+var (
+	ErrQuote      = errors.New("excess or missing \" in quoted-field")
+	ErrFieldCount = errors.New("wrong number of fields")
+)
+
+func ReadLine(r io.Reader) (string, error) {
+}
+
+func GetField(n int) (string, error) {
+}
+
+func GetNumberOfFields() int {
+}
