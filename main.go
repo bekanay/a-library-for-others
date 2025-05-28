b8849f66268fc5f872255b4ed0985400b2b7551c
bekanay <beki.bekovich2004@gmail.com>
2025-05-28 20:04:02 +0500

    functionality is working but not tested

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,18 +17,26 @@ func main() {
 	}
 	defer file.Close()
 
-	var csvparser CSVParser
-
+	var csvparser CSVParser = &MyCSVParser{
+		reader: bufio.NewReader(file),
+	}
 	for {
 		line, err := csvparser.ReadLine(file)
 		if err != nil {
 			if err == io.EOF {
+				if len(line) > 0 {
+					fmt.Println(line)
+				}
 				break
 			}
 			fmt.Println("Error reading line:", err)
 			return
 		}
 		fmt.Println(line)
+		fmt.Println(csvparser.GetField(1))
+		fmt.Println(csvparser.GetField(2))
+		fmt.Println(csvparser.GetField(3))
+		fmt.Println(csvparser.GetNumberOfFields())
 	}
 }
 
@@ -41,11 +51,61 @@ var (
 	ErrFieldCount = errors.New("wrong number of fields")
 )
 
-func ReadLine(r io.Reader) (string, error) {
+type MyCSVParser struct {
+	reader *bufio.Reader
+	fields []string
 }
 
-func GetField(n int) (string, error) {
+func (p *MyCSVParser) ReadLine(r io.Reader) (string, error) {
+	line, err := p.reader.ReadString('\n')
+	if err != nil {
+		if err == io.EOF && len(line) > 0 {
+			line = strings.TrimSpace(line)
+			return line, io.EOF
+		}
+		return "", err
+	}
+	line = strings.TrimSpace(line)
+	p.fields, err = splitLine(line)
+	if err != nil {
+		return "", err
+	}
+	return line, nil
 }
 
-func GetNumberOfFields() int {
+func (p *MyCSVParser) GetField(n int) (string, error) {
+	if n < 0 || n > len(p.fields) {
+		return "", ErrFieldCount
+	}
+	field := p.fields[n-1]
+	return field, nil
+}
+
+func (p *MyCSVParser) GetNumberOfFields() int {
+	return len(p.fields)
+}
+
+func splitLine(line string) ([]string, error) {
+	fields := []string{}
+	isQuote := false
+	word := ""
+	for i := 0; i < len(line); i++ {
+		if line[i] == '"' {
+			if !isQuote {
+				isQuote = true
+			} else {
+				fields = append(fields, word)
+			}
+		} else if line[i] == ',' && !isQuote {
+			fields = append(fields, word)
+			word = ""
+		} else {
+			word += string(line[i])
+		}
+	}
+	if isQuote {
+		return nil, ErrQuote
+	}
+	fields = append(fields, word)
+	return fields, nil
 }
